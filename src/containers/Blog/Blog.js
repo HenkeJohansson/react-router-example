74a1175eec3cbc7d8768d2402f7c4da5540c1f66
Henrik Johansson <henrik89johansson@gmail.com>
2018-02-24 12:11:14 +0100

    Prepare for route

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+
+class Blog extends Component {
+
+    render () {
+        
+        return (
+            <div className="Blog">
+            <header>
+                <nav>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/new-post">New Post</a></li>
+                </nav>
+            </header>
+            <Posts />
+            </div>
+        );
+    }
+}
+
+export default Blog;
