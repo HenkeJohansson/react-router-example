c3dbb211d6eacbc642beaf10308d86b48cfe13e7
Henrik Johansson <henrik89johansson@gmail.com>
2018-03-02 07:47:57 +0100

    Redirecting with history is better.

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -25,17 +25,13 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
-                this.setState({submitted: true});
+                this.props.history.push('/posts');
             });
     }
 
     render () {
-        let redirect = null;
-        if(this.state.submitted) 
-            redirect = <Redirect to="/posts" />
         return (
             <div className="NewPost">
-                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
